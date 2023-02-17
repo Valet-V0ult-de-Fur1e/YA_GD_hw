5b07680905cbb658820dd1fa2ddd51a4ba5915df
Valet-V0ult-de-Fur1e <egor.a.mironov@gmail.com>
2023-02-17 14:21:35 +1000

    a/d - add force

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -5,10 +5,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Rigidbody2D rb2D;
+    public float thrust = 20f;
+    public float start_force = 1500f;
+
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        rb2D.AddForce(new Vector2(-start_force, 0));
+    }
     void FixedUpdate()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0)) {
             Destroy(GetComponent<HingeJoint2D>());
+        }
+        if (Input.GetKeyDown(KeyCode.D)) {
+            rb2D.AddForce(new Vector2(thrust, 0));
+        }
+        if (Input.GetKeyDown(KeyCode.A)) {
+            rb2D.AddForce(new Vector2(-thrust, 0));
         }
     }
 }
